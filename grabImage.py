e5e25fbc442dc30a12b3728833246ae96523f721
marsolmos <m.marsolmos@gmail.com>
2020-06-13 14:31:23 +0200

    key input to game added

--- grabImage.py
+++ grabImage.py
@@ -2,19 +2,34 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 import time
+import pyautogui
+from directkeys import PressKey, W, A, S, D
 
 
-def screen_record():
-    last_time = time.time()
+def process_img(image):
+    original_image = image
+    # convert to gray
+    processed_img = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # edge detection
+    processed_img = cv2.Canny(processed_img, threshold1=100, threshold2=150)
+    return processed_img
+
+
+def main():
+    for i in list(range(4))[::-1]:
+        print(i+1)
+        time.sleep(1)
+
     while(True):
+        PressKey(W)
         # 800x600 windowed mode
-        printscreen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
-        print('loop took {} seconds'.format(time.time()-last_time))
-        last_time = time.time()
-        cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        screen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+        new_screen = process_img(screen)
+        cv2.imshow('window', new_screen)
+        # cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
 
 if __name__ == '__main__':
-    screen_record()
+    main()
