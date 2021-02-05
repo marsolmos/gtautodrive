b2787e3eb14f67291cbac0a3f7cd938f4c954859
marsolmos <m.marsolmos@gmail.com>
2021-02-05 17:21:58 +0100

    modify 2_balance_data.py
    Modify step 2 of the pipeline to include inthe same script the extension and the balance of the data

--- 2_balance_data.py
+++ 2_balance_data.py
@@ -4,8 +4,40 @@ import pandas as pd
 from collections import Counter
 from random import shuffle
 
+"""
+Extend and Balance Data collected in step 1
+================================
+"""
+
+# %%
+# Extend the existing data
+# ~~~~~~~~~~~~~~~~~~~~~~~~~
+# Duplicate existing data in MODEL_NAME_LOAD and duplicate it into
+# the same folder
 MODEL_NAME_LOAD = 'model_3_400x300_raw_custom' # Load raw data from this path
 MODEL_NAME_SAVE = 'model_3_400x300_balanced_custom' # Save balanced data in this path
+
+file_path = 'D:/Data Warehouse/pygta5/data/{}'.format(MODEL_NAME_LOAD)
+FILE_I_END = len(os.listdir(file_path))
+
+NEW_INDEX_SAVE = FILE_I_END
+
+for i in range(1,FILE_I_END+1):
+    NEW_INDEX_SAVE += 1
+
+    file_name = 'D:/Data Warehouse/pygta5/data/{}/training_data-{}.npy'.format(MODEL_NAME_LOAD, i)
+    train_data = np.load(file_name, allow_pickle=True)
+    save_name = 'D:/Data Warehouse/pygta5/data/{}/training_data-{}.npy'.format(MODEL_NAME_LOAD, NEW_INDEX_SAVE)
+    np.save(save_name, train_data)
+
+    print('File saved --> Original index: {}  -  New index: {} '.format(i, NEW_INDEX_SAVE))
+
+
+# %%
+# Balance the extended data
+# ~~~~~~~~~~~~~~~~~~~~~~~~~
+# Balance extented data in MODEL_NAME_LOAD path and
+# save it again in MODEL_NAME_LOAD folder
 
 file_path = 'D:/Data Warehouse/pygta5/data/{}'.format(MODEL_NAME_LOAD)
 FILE_I_END = len(os.listdir(file_path))
